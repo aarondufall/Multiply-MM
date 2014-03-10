494b3c553e8a4b6aed50cd7cc53d4f21f8b5e0b0
Aaron Dufall <aald212@gmail.com>
2014-03-10 16:25:40 -0700

    Initial Commit

---
+++ Multiply/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Multiply
+//
+//  Created by Aaron Dufall on 10/03/2014.
+//  Copyright (c) 2014 Aaron Dufall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Multiply/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Multiply
+//
+//  Created by Aaron Dufall on 10/03/2014.
+//  Copyright (c) 2014 Aaron Dufall. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
